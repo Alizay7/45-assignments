bf4f6326274502df8d0528cacade2bedcb80a900
Alizay7 <162825228+Alizay7@users.noreply.github.com>
2024-03-31 20:17:56 +0500

    assignment 20

---
+++ q20.js
@@ -0,0 +1,3 @@
+var programmingLanguages = ["typescript", "javascript", "python"];
+console.log("list of programmingLanguages");
+programmingLanguages.forEach(function (language) { return console.log(language); });
