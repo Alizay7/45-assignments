9edeb556e5e1194caef665e88cb5cce24a08bcbc
Alizay7 <162825228+Alizay7@users.noreply.github.com>
2024-03-31 20:07:08 +0500

    assignment 19 dinner guest

---
+++ q19.js
@@ -0,0 +1,4 @@
+var guestList = ["Aliza", "Rehana", "Safdar"];
+//guestList.forEach(oneGuest => console.log(`salam ${oneGuest},would you like to dinner with me`));
+var lengthGuests = guestList.length;
+console.log("we are inviting total ".concat(lengthGuests, "guests"));
