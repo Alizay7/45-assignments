bba365ca456a33d0c135d3ec1fff317293bc815b
Alizay7 <162825228+Alizay7@users.noreply.github.com>
2024-03-31 20:33:26 +0500

    assignment 21 typescript objects

---
+++ q21.ts
@@ -0,0 +1,11 @@
+interface itcourse {
+    courseName: string;
+    onsiteStudents:number;
+
+}
+let itcourse = {
+    courseName :"typescript and javascript",
+    location: "governor house sindh",
+    onsiteStudents : 50000
+};
+console.log(itcourse);
