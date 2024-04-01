f8b41e91e882d588447da82ebcf2c6ca84d3a324
Alizay7 <162825228+Alizay7@users.noreply.github.com>
2024-04-01 06:05:05 +0500

    assignment 22

---
+++ q22.js
@@ -0,0 +1,6 @@
+//creating a array
+var errorArray = ["A", "B", "C", "D"];
+//producing error by assessing invalid index of array
+//console.log(errorArray[10]);
+//error removed
+console.log(errorArray[1]);
