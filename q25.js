050027cc517118d369ed00c46ec17779bb7c8362
Alizay7 <162825228+Alizay7@users.noreply.github.com>
2024-04-01 08:06:43 +0500

    alien color

---
+++ q25.js
@@ -0,0 +1,8 @@
+//let alienColour = "green"
+var alienColour = "white";
+if (alienColour === "green") {
+    console.log("player earn 5 points");
+}
+if (alienColour === "white") {
+    console.log("player earn 6 points");
+}
