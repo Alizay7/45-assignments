6e9bf761b1076cb606e5e29e71bc85427d656221
Alizay7 <162825228+Alizay7@users.noreply.github.com>
2024-04-01 06:37:07 +0500

    assignment 23 conditional test

---
+++ q23.ts
@@ -0,0 +1,23 @@
+//making variable
+let five = 5;
+let ten = 10;
+//test 01
+console.log(five == 5);
+//test 02
+console.log(ten ==10);
+//test 03
+console.log(five!= ten);
+//test 04
+console.log(ten>five);
+//test 05
+console.log(five<ten);
+//test 06
+console.log(ten<five);
+//test07
+console.log(five==ten);
+//test 08
+console.log(9<7);
+//test 09
+console.log(five>ten);
+//test 10
+console.log(100<50);
